e1909a6bca5d085eeec1e11b94584869b29dddb8
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2023-03-29 02:29:10 +0530

    move to directories

--- P2_Shapes/p2_dataloader.py
+++ P2_Shapes/p2_dataloader.py
@@ -5,16 +5,31 @@ DATA_PATH = '/storage/niranjan.rajesh_ug23/DCV_data/Shapes2D/output'
 
 shape_count = {}
 
-for file in os.listdir(DATA_PATH):
-    shape = file.split('_')[0]
-    
-    if shape not in shape_count.keys():
-        shape_count[shape] = 1
-    else:
-        shape_count[shape] += 1
-    
-    new_name = shape + '_' + str(shape_count[shape]).zfill(4) + '.png'
-    src = os.path.join(DATA_PATH, file)
-    dest = os.path.join(DATA_PATH, new_name)
-    os.rename(src, dest)
-    
+def rename_files():
+    for file in os.listdir(DATA_PATH):
+        shape = file.split('_')[0]
+        
+        if shape not in shape_count.keys():
+            shape_count[shape] = 1
+        else:
+            shape_count[shape] += 1
+        
+        new_name = shape + '_' + str(shape_count[shape]).zfill(4) + '.png'
+        src = os.path.join(DATA_PATH, file)
+        dest = os.path.join(DATA_PATH, new_name)
+        os.rename(src, dest)
+
+def make_subdirs():
+    # create a folder for each class in the dataset and move the images to the corresponding folder
+    for file in os.listdir(DATA_PATH):
+        shape = file.split('_')[0]
+        src = os.path.join(DATA_PATH, file)
+        dest = os.path.join(DATA_PATH, shape)
+
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+        os.rename(src, os.path.join(dest, file))
+        
+if __name__ == "__main__":
+    # rename_files()
+    make_subdirs()
