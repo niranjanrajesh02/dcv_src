a7a46b257a00a30009f7601e6f10da1f23a5faac
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2023-03-29 18:05:45 +0530

    layer compat

--- P1_Edges/p1_model.py
+++ P1_Edges/p1_model.py
@@ -1,10 +1,10 @@
 from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Input, Conv2DTranspose
 from keras.models import Sequential
 
-model = Sequential()
-
-# Add convolutional layers to extract edges
 # model.add(Input(shape=(256,256,3)))
+
+model = Sequential()
+# Add convolutional layers to extract edges
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(None, None, 3)))
 model.add(MaxPooling2D((2, 2), padding='same'))
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
@@ -12,9 +12,10 @@ model.add(MaxPooling2D((2, 2), padding='same'))
 
 
 # Add upsampling layers to reconstruct edge map
-
 model.add(Conv2DTranspose(32, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
 model.add(Conv2DTranspose(32, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(1, (3, 3), activation='sigmoid', padding='same'))
 
 # Compile the model
