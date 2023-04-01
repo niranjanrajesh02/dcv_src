336a94e85c24d75776b7f79aaf96606cf7625106
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2023-04-01 16:27:04 +0530

    model bug

--- P2_Shapes/p2_main.py
+++ P2_Shapes/p2_main.py
@@ -9,7 +9,7 @@ train_data, valid_data, class_names = make_dataset()
 results_path = '/home/niranjan.rajesh_ug23/DCV/dcv_src/P2_Shapes/Results'
 
 
-early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=10, restore_best_weights=True)
 model = build_model(len(class_names))
 history = model.fit(train_data, validation_data=valid_data, epochs=100, batch_size=32, verbose=1, callbacks=[early_stop])
 hist_path = results_path+'/history.npy'
