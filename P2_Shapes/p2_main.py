8887e5118cd2bbed0233bd9d863720c045abd01c
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2023-04-08 15:48:18 +0530

    callback

--- P2_Shapes/p2_main.py
+++ P2_Shapes/p2_main.py
@@ -25,15 +25,15 @@ results_path = '/home/niranjan.rajesh_ug23/DCV/dcv_src/P2_Shapes/Results'
 
 early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=10, restore_best_weights=True)
 model = build_model(len(class_names), args)
-# history = model.fit(train_data, validation_data=valid_data, epochs=args.epochs, batch_size=args.batch_size, verbose=1, callbacks=[])
-# hist_path = results_path+'/history.npy'
-# np.save(hist_path,history.history)
-# print("History saved to: ", hist_path)
+history = model.fit(train_data, validation_data=valid_data, epochs=args.epochs, batch_size=args.batch_size, verbose=1, callbacks=[early_stop])
+hist_path = results_path+'/history.npy'
+np.save(hist_path,history.history)
+print("History saved to: ", hist_path)
 
-# # summarize history for loss and accuracy
-# plot_loss(history)
-# plot_accuracy(history)
+# summarize history for loss and accuracy
+plot_loss(history)
+plot_accuracy(history)
 
-# model_path = results_path+'/p2_model.h5'
-# model.save(model_path)
-# print("Model saved to: ", model_path)
+model_path = results_path+'/p2_model.h5'
+model.save(model_path)
+print("Model saved to: ", model_path)
